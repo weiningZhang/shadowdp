d1e7e88912ef21ff43db161fe9dd3a65aecf78f8
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-04-02 12:27:15 -0400

    Add rewrite version for partialsum.

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -34,6 +34,8 @@ shadowdp verify examples/transformed/numsparsevectorN.c
 echo ''
 shadowdp verify examples/transformed/partialsum.c
 echo ''
+shadowdp verify examples/transformed/partialsum_rewrite.c
+echo ''
 shadowdp verify examples/transformed/prefixsum.c
 echo ''
 shadowdp verify examples/transformed/smartsum.c

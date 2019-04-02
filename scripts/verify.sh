f8527d18c9c2d8cfd98cbb4a735a2cbaa6b5eb4b
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-04-02 13:06:19 -0400

    Add rewrite version for prefixsum.

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -38,6 +38,8 @@ shadowdp verify examples/transformed/partialsum_rewrite.c
 echo ''
 shadowdp verify examples/transformed/prefixsum.c
 echo ''
+shadowdp verify examples/transformed/prefixsum_rewrite.c
+echo ''
 shadowdp verify examples/transformed/smartsum.c
 echo ''
 shadowdp verify examples/transformed/smartsum_rewrite.c

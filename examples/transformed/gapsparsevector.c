195e4a46af46673a169fde565c76231d541408b6
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-04-15 16:58:59 -0400

    Change from float NN to int NN.
    CPAChecker has fixed the type conversion problem.

--- examples/transformed/gapsparsevector.c
+++ examples/transformed/gapsparsevector.c
@@ -7,7 +7,7 @@ extern void __assert_fail();
 #define Abs(x) ((x) < 0 ? -(x) : (x))
 typedef enum { false = 0, true = 1 } bool;
     
-int gapsparsevector(float epsilon, int size, float q[], float T, float NN, float __SHADOWDP_ALIGNED_DISTANCE_q[], float __SHADOWDP_SHADOW_DISTANCE_q[])
+int gapsparsevector(float epsilon, int size, float q[], float T, int NN, float __SHADOWDP_ALIGNED_DISTANCE_q[], float __SHADOWDP_SHADOW_DISTANCE_q[])
 {
   __VERIFIER_assume(epsilon > 0);
   __VERIFIER_assume(size > 0);
@@ -18,14 +18,14 @@ int gapsparsevector(float epsilon, int size, float q[], float T, float NN, float
   float eta_1 = __VERIFIER_nondet_float();
   __SHADOWDP_v_epsilon = __SHADOWDP_v_epsilon + (0.5 * NN);
   float T_bar = T + eta_1;
-  float count = 0;
+  int count = 0;
   int i = 0;
-  while ((count <= (NN - 1)) && (i < size))
+  while ((count < NN) && (i < size))
   {
     __VERIFIER_assume(__SHADOWDP_ALIGNED_DISTANCE_q[i] <= 1);
     __VERIFIER_assume(__SHADOWDP_ALIGNED_DISTANCE_q[i] >= -1);
     __VERIFIER_assume(__SHADOWDP_SHADOW_DISTANCE_q[i] == __SHADOWDP_ALIGNED_DISTANCE_q[i]);
-    __VERIFIER_assert((count <= (NN - 1)) && (i < size));
+    __VERIFIER_assert((count < NN) && (i < size));
     float eta_2 = __VERIFIER_nondet_float();
     __SHADOWDP_v_epsilon = __SHADOWDP_v_epsilon + (((q[i] + eta_2) >= T_bar) ? (0.25 * Abs(__SHADOWDP_ALIGNED_DISTANCE_q[i] - 1)) : (0));
     if ((q[i] + eta_2) >= T_bar)

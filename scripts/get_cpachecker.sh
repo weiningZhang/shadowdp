e213b7f1100e27a2201b1df04218565f98eb37bc
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-05-08 23:07:27 -0400

    Use bash instead of sh.

--- scripts/get_cpachecker.sh
+++ scripts/get_cpachecker.sh
@@ -19,8 +19,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-#!/bin/sh
--set e
+#!/bin/bash
+set e
 git clone https://github.com/sosy-lab/cpachecker.git cpachecker-build
 cd cpachecker-build
 git reset --hard 57e456aad032bcb2e42911202976423b785797cb
